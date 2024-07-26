c472ee13c3569d2d5a513372983ed422384400e2
Helena Kohl <helenachristina@gmx.de>
2024-07-26 11:10:28 +0200

    removed welcome

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -1,19 +1,12 @@
-import { assistantProfile } from "../assets/assitantProfile";
-
 export const Welcome: React.FC = () => (
-  <div className="flex flex-col items-start">
+  <div className="bg-white border-gray-100 border-2 rounded-lg px-8 py-5 mr-20 w-full">
     <div className="flex items-center mb-2">
-      <img
-        src={assistantProfile.image}
-        alt="Assistant"
-        className="w-14 h-14 rounded-full mr-4"
-      />
-      <p className="text-center">{assistantProfile.name}</p>
+      <h1 className="text-2xl font-bold">
+        Welcome to BMW.
+      </h1>
     </div>
-    <div className="bg-gray-100 border-gray-300 border-2 rounded-lg p-2 mr-20 w-full">
-      <p>
-        Hey there! Welcome to BMW. I'm {assistantProfile.name}, your sales assistant. Are you looking for something specific today, or would you like some suggestions? Whether you have questions about models, features, or financing, I've got you covered. How can I assist you?
-      </p>
-    </div>
+    <p>
+      Welcome Message
+    </p>
   </div>
 );
